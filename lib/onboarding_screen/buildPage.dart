0859f2a06e23b4de91f49f8938aa8295d408ef8d
anonymousdomain <dawityitagesu@gmail.com>
2023-03-16 21:39:07 +0300

    packages

---
+++ lib/onboarding_screen/buildPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BuildPage extends StatelessWidget {
+  const BuildPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:  Padding(padding: EdgeInsets.all(20),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+          ],
+        ),
+      ),),
+    );
+  }
+}
---
+++ lib/onboarding_screen/onboarding.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final PageController _pageController = PageController();
+
+  List<Widget> screens = [
+    
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
