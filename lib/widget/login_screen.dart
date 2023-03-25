a3de2c1848370ef0d2cfe49ac7e780c3d5f62d67
anonymousdomain <dawityitagesu@gmail.com>
2023-03-25 19:56:55 +0300

    login screen

--- lib/widget/login_screen.dart
+++ lib/widget/login_screen.dart
@@ -6,7 +6,9 @@ import 'package:country_code_picker/country_code_picker.dart';
 import 'package:equb/helper/auth_service.dart';
 import 'package:equb/provider/auth_state.dart';
 import 'package:equb/utils/theme.dart';
+import 'package:equb/widget/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:otp_text_field/otp_field.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:provider/provider.dart';
@@ -30,7 +32,7 @@ class LoginScreenState extends State<LoginScreen> {
 
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _phoneNumberController = TextEditingController();
-  final TextEditingController _pinCodeController = TextEditingController();
+  // final TextEditingController _pinCodeController = TextEditingController();
 
   AuthService authService = AuthService(AuthState());
 
@@ -38,159 +40,130 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         actions: [
           IconButton(
               onPressed: (() {
                 currentTheme.toggleTheme();
               }),
-              icon: Icon(Icons.brightness_1))
+              icon: Icon(FeatherIcons.moon))
         ],
       ),
       body: Center(
-        child: Consumer<AuthState>(builder: (context, auth, child) {
-          return SingleChildScrollView(
-            padding: EdgeInsets.all(20),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  auth.status == AuthStatus.uninitialized
-                      ? Column(
-                          children: [
-                            Text(
-                              'Your Phone Number',
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      ?.color),
-                            ),
-                            Text(
-                              'please confirm your country code and enter your phone number',
-                              softWrap: true,
-                              style: TextStyle(
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .headline1!
-                                      .color),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: TextFormField(
-                                    style: TextStyle(
-                                        color: Theme.of(context)
-                                            .textTheme
-                                            .headline1!
-                                            .color),
-                                    controller: _phoneNumberController,
-                                    keyboardType: TextInputType.phone,
-                                    decoration: InputDecoration(
-                                      prefixIcon: CountryCodePicker(
-                                        initialSelection: 'ET',
-                                        favorite: const ['+251', 'ET'],
-                                        showCountryOnly: false,
-                                        showOnlyCountryWhenClosed: false,
-                                        onChanged: (value) {
-                                          setState(() {
-                                            _selectedCountry = value.dialCode;
-                                          });
-                                        },
-                                      ),
-                                      hintText: 'Enter phone number',
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                        borderSide: BorderSide(
-                                            color: Colors.indigo, width: 2),
-                                      ),
-                                    ),
-                                    validator: ((value) {
-                                      if (value!.isEmpty) {
-                                        return 'please enter your phone number';
-                                      }
-                                      return null;
-                                    }),
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              height: 16,
-                            ),
-                            ElevatedButton(
-                              onPressed: () async {
-                                if (_formKey.currentState!.validate()) {
-                                  log(_selectedCountry! +
-                                      _phoneNumberController.text.trim());
-
-                                  await Provider.of<AuthState>(context,
-                                          listen: false)
-                                      .verifyPhoneNumber(_selectedCountry! +
-                                          _phoneNumberController.text.trim())
-                                      .then((value) => {
-                                            auth.setStatus(
-                                                AuthStatus.authenticating)
-                                          });
-                                }
-                              },
-                              child: Text('submit'),
-                            )
-                          ],
-                        )
-                      : Column(
-                          children: [
-                            auth.status == AuthStatus.authenticating
-                                ? CircularProgressIndicator()
-                                : Column(
-                                    children: [
-                                      PinCodeTextField(
-                                        appContext: context,
-                                        length: 6,
-                                        onChanged: (value) {
-                                          _pinCodeController.text = value;
-                                        },
-                                        textStyle: TextStyle(
-                                            color: currentTheme.currentTheme ==
-                                                    ThemeMode.dark
-                                                ? Colors.white
-                                                : Colors.black),
-                                        keyboardType: TextInputType.number,
-                                        pinTheme: PinTheme(
-                                            shape: PinCodeFieldShape.box,
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            fieldHeight: 50,
-                                            fieldWidth: 40),
-                                      ),
-                                      ElevatedButton(
-                                        onPressed: () async {
-                                          log(auth.verificationId);
-                                          log(_pinCodeController.text);
-                                          await Provider.of<AuthState>(context,
-                                                  listen: false)
-                                              .signInwithPhoneNumber(
-                                                  auth.verificationId,
-                                                  _pinCodeController.text).then((value) => {
-                                                    log(auth.status.toString())
-                                                  });
-                                        },
-                                        child: Text('verify'),
-                                      ),
-                                    ],
-                                  ),
-                          ],
-                        )
-                ],
-              ),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(30),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(
+                  textAlign: TextAlign.center,
+                  'Your Phone Number',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).textTheme.bodyText1?.color),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  textAlign: TextAlign.center,
+                  'please confirm your country code and enter your phone number',
+                  softWrap: true,
+                  style: TextStyle(
+                      color: Theme.of(context)
+                          .textTheme
+                          .headline1!
+                          .color
+                          ?.withOpacity(0.4)),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  // width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextFormField(
+                    style: TextStyle(
+                        color: Theme.of(context).textTheme.headline1!.color),
+                    controller: _phoneNumberController,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      focusColor: Theme.of(context).primaryColor,
+                      prefixIcon: CountryCodePicker(
+                        searchStyle: TextStyle(
+                            color: currentTheme.currentTheme == ThemeMode.dark
+                                ? Colors.white
+                                : Colors.black,
+                            backgroundColor:
+                                Theme.of(context).scaffoldBackgroundColor),
+                        initialSelection: 'ET',
+                        favorite: const ['+251', 'ET'],
+                        showCountryOnly: false,
+                        showOnlyCountryWhenClosed: false,
+                        textStyle: TextStyle(
+                          color: Theme.of(context).textTheme.headline1!.color,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedCountry = value.dialCode;
+                          });
+                        },
+                        dialogBackgroundColor:
+                            Theme.of(context).scaffoldBackgroundColor,
+                        dialogTextStyle: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.headline1!.color),
+                        boxDecoration: BoxDecoration(),
+                      ),
+                      hintText: 'Enter phone number',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.indigo, width: 2),
+                      ),
+                    ),
+                    validator: ((value) {
+                      if (value!.isEmpty) {
+                        return 'please enter your phone number';
+                      }
+                      return null;
+                    }),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  widthFactor: 6,
+                  child: GestureDetector(
+                      child: CircleAvatar(
+                        maxRadius: 25,
+                        backgroundColor: Theme.of(context).primaryColor,
+                        child: Icon(
+                          FeatherIcons.arrowRight,
+                          size: 30,
+                          color: Theme.of(context).textTheme.headline1!.color,
+                        ),
+                      ),
+                      onTap: () async {
+                        if (_formKey.currentState!.validate()) {
+                          log(_selectedCountry! + _phoneNumberController.text);
+                          await Provider.of<AuthState>(context, listen: false)
+                              .verifyPhoneNumber(_selectedCountry! +
+                                  _phoneNumberController.text)
+                              .then((value) => Provider.of<AuthState>(context,listen: false)
+                                  .setStatus(AuthStatus.authenticating));
+                        }
+                      }),
+                )
+              ],
             ),
-          );
-        }),
+          ),
+        ),
       ),
     );
   }
