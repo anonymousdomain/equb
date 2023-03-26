0bf4135e6f2e123a265748409b185f855e516e2e
anonymousdomain <dawityitagesu@gmail.com>
2023-03-26 22:05:33 +0300

    otp Field button

--- lib/widget/otp_field.dart
+++ lib/widget/otp_field.dart
@@ -43,116 +43,136 @@ class _OtpFieldState extends State<OtpField> {
   Widget build(BuildContext context) {
     AuthState provider = context.watch<AuthState>();
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        actions: [
-          IconButton(
-              onPressed: (() {
-                currentTheme.toggleTheme();
-              }),
-              icon: Icon(FeatherIcons.moon))
-        ],
-      ),
-      body: Center(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          actions: [
+            IconButton(
+                onPressed: (() {
+                  currentTheme.toggleTheme();
+                }),
+                icon: Icon(FeatherIcons.moon))
+          ],
+        ),
+        body: Center(
           child: SingleChildScrollView(
-        padding: EdgeInsets.all(30),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Text(
-                textAlign: TextAlign.center,
-                'Check Your Message',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Theme.of(context).textTheme.bodyText1!.color),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
+            padding: EdgeInsets.all(30),
+            child: Form(
+              key: _formKey,
+              child: Column(
                 children: [
                   Text(
-                    "we've sent the code to ",
+                    textAlign: TextAlign.center,
+                    'Check Your Message',
                     style: TextStyle(
-                      color: Theme.of(context)
-                          .textTheme
-                          .headline1!
-                          .color
-                          ?.withOpacity(0.4),
-                    ),
+                        fontSize: 20,
+                        color: Theme.of(context).textTheme.bodyText1!.color),
                   ),
-                  Text(
-                    provider.phoneNumber,
-                    style: TextStyle(
-                      color: Theme.of(context)
-                          .textTheme
-                          .headline1!
-                          .color
-                          ?.withOpacity(0.4),
-                    ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "we've sent the code to ",
+                        style: TextStyle(
+                          color: Theme.of(context)
+                              .textTheme
+                              .headline1!
+                              .color
+                              ?.withOpacity(0.4),
+                        ),
+                      ),
+                      Text(
+                        provider.phoneNumber,
+                        style: TextStyle(
+                          color: Theme.of(context)
+                              .textTheme
+                              .headline1!
+                              .color
+                              ?.withOpacity(0.4),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  PinCodeTextField(
+                    appContext: context,
+                    length: 6,
+                    onChanged: (value) {
+                      _pinCodeController.text = value;
+                    },
+                    textStyle: TextStyle(
+                        color: currentTheme.currentTheme == ThemeMode.dark
+                            ? Colors.white
+                            : Colors.black),
+                    keyboardType: TextInputType.number,
+                    pinTheme: PinTheme(
+                        shape: PinCodeFieldShape.box,
+                        borderRadius: BorderRadius.circular(10),
+                        fieldHeight: 50,
+                        fieldWidth: 40),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return '';
+                      }
+                      if (value.isNotEmpty && value.length != 6) {
+                        return '';
+                      }
+                      return null;
+                    },
+                    onCompleted: (value) {
+                      if (value.length == 6) {
+                        signIn();
+                      }
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                        child: Text(
+                          ' code will expired in - ',
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .color
+                                ?.withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                        child: Text(
+                          '${provider.countdown} sec',
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .color
+                                ?.withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
                   ),
                 ],
               ),
-              SizedBox(
-                height: 15,
-              ),
-              PinCodeTextField(
-                appContext: context,
-                length: 6,
-                onChanged: (value) {
-                  _pinCodeController.text = value;
-                },
-                textStyle: TextStyle(
-                    color: currentTheme.currentTheme == ThemeMode.dark
-                        ? Colors.white
-                        : Colors.black),
-                keyboardType: TextInputType.number,
-                pinTheme: PinTheme(
-                    shape: PinCodeFieldShape.box,
-                    borderRadius: BorderRadius.circular(10),
-                    fieldHeight: 50,
-                    fieldWidth: 40),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return '';
-                  }
-                  if (value.isNotEmpty && value.length != 6) {
-                    return '';
-                  }
-                  return null;
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  SizedBox(
-                    height: 20,
-                    child: Text('sms code will expired in ',
-                        style:
-                            TextStyle(color: Theme.of(context).primaryColor)),
-                  ),
-                  SizedBox(
-                    height: 20,
-                    child: Text(
-                      '${provider.countdown} sec',
-                      style: TextStyle(
-                          fontSize: 16, color: Theme.of(context).primaryColor),
-                    ),
-                  ),
-                ],
-              ),
-              provider.status == AuthStatus.codeAutoRetievalTimeout
-                  ? CustomButton(title: 'resend', onTap: resend)
-                  : CustomButton(title: 'signin', onTap: signIn)
-            ],
+            ),
           ),
         ),
-      )),
-    );
+        bottomSheet: provider.status == AuthStatus.codeAutoRetievalTimeout
+            ? CustomButton(title: 'signin', onTap: signIn)
+            : CustomButton(title: 'resend', onTap: resend));
   }
 }
