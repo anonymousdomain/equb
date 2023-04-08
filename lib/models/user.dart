aa84d7eeb1a2cf88dc427df48aa6646661239ab6
anonymousdomain <dawityitagesu@gmail.com>
2023-04-08 09:30:39 +0300

    user model from map

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -33,7 +33,19 @@ class User {
       imageUrl: doc.get('imageUrl'),
     );
   }
-
+  factory User.fromMap(Map<String, dynamic> map, {String? id}) {
+    return User(
+      id: id,
+      firstName: map['firstName'],
+      lastName: map['lastName'],
+      bankName: map['bankName'],
+      bankNumber: map['bankNumber'],
+      phoneNumber: map['phoneNumber'],
+      imageUrl: map['imageUrl'],
+      uid: map['uid'],
+    );
+  }
+ 
   //convert user instance to map
   Map<String, dynamic> toMap() {
     return {
