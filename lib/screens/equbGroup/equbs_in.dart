e94cfb15533b51b8e231ea17f4c9964a51b579e5
anonymousdomain <dawityitagesu@gmail.com>
2023-04-10 00:09:17 +0300

    equb you in card refacterd

--- lib/screens/equbGroup/equbs_in.dart
+++ lib/screens/equbGroup/equbs_in.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equb/helper/firbasereference.dart';
-import 'package:equb/main.dart';
 import 'package:equb/service/group.dart';
 import 'package:equb/models/equb.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:intl/intl.dart';
 
 class GroupsIn extends StatefulWidget {
@@ -40,18 +40,15 @@ class _GroupsInState extends State<GroupsIn> {
             return GridView.builder(
                 itemCount: docs.length,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, childAspectRatio: 0.7),
+                    crossAxisCount: 1, childAspectRatio:1.0),
                 itemBuilder: (context, index) {
-                
-
-                  // final doc = EqubModel.fromDocument(docs[index]);
-
                   return Card(
+                    margin: EdgeInsets.all(10),
                     elevation: 4,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10)),
                     child: Padding(
-                      padding: const EdgeInsets.all(16.0),
+                      padding: const EdgeInsets.all(16),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         mainAxisAlignment: MainAxisAlignment.start,
@@ -59,57 +56,33 @@ class _GroupsInState extends State<GroupsIn> {
                           Text(
                             docs[index].get('groupName'),
                             style: TextStyle(
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .headline1!
-                                    .color,
+                                color: Theme.of(context).primaryColor,
                                 fontWeight: FontWeight.bold,
                                 fontSize: 20),
+                            textAlign: TextAlign.center,
                           ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            docs[index].get('catagory'),
-                            style: TextStyle(
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            docs[index].get('equbType'),
-                            style: TextStyle(
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            docs[index].get('moneyAmount'),
-                            style: TextStyle(
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            docs[index].get('roundSize'),
-                            style: TextStyle(
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('MMMM yyyy')
+                          CustomListTile(
+                              icon: FeatherIcons.feather,
+                              lead: 'EqubCatagory',
+                              value: docs[index].get('catagory')),
+                          CustomListTile(
+                              icon: FeatherIcons.activity,
+                              lead: 'EqubType',
+                              value: docs[index].get('equbType')),
+                          CustomListTile(
+                              icon: FeatherIcons.dollarSign,
+                              lead: 'MoneyAmount',
+                              value: docs[index].get('moneyAmount')),
+                          CustomListTile(
+                              icon: FeatherIcons.users,
+                              lead: 'total members',
+                              value: docs[index].get('members').toList().length.toString()),
+                          CustomListTile(
+                            icon: FeatherIcons.calendar,
+                            lead: 'Schedule',
+                            value: DateFormat('MMMM dd yyyy')
                                 .format(docs[index].get('createdAt').toDate())
                                 .toString(),
-                            style: TextStyle(
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                              fontSize: 16,
-                            ),
                           ),
                         ],
                       ),
@@ -117,6 +90,37 @@ class _GroupsInState extends State<GroupsIn> {
                   );
                 });
           }),
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  String lead;
+  String value;
+  IconData icon;
+  CustomListTile({
+    Key? key,
+    required this.lead,
+    required this.value,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      autofocus: true,
+      // horizontalTitleGap: 0.1,
+      leading: Icon(icon),
+      title: Text(
+        lead,
+        style: TextStyle(
+            color: Theme.of(context).textTheme.headline1!.color),
+      ),
+      trailing: Text(
+        value,
+        style: TextStyle(
+            color: Theme.of(context).textTheme.headline1!.color),
+      ),
     );
   }
 }
