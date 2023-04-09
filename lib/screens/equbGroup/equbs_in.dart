5aa0de7fff694ea73f678775f3a54801087d4b7c
anonymousdomain <dawityitagesu@gmail.com>
2023-04-09 19:05:10 +0300

    Grid View

--- lib/screens/equbGroup/equbs_in.dart
+++ lib/screens/equbGroup/equbs_in.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equb/helper/firbasereference.dart';
+import 'package:equb/main.dart';
 import 'package:equb/service/group.dart';
-import 'package:equb/widget/nav_drawer.dart';
+import 'package:equb/models/equb.dart';
 import 'package:flutter/material.dart';
 
 class GroupsIn extends StatefulWidget {
@@ -23,7 +26,31 @@ class _GroupsInState extends State<GroupsIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('groups in'),
+      body: FutureBuilder(
+          future:
+              groupCollection.where('members', arrayContains: user?.uid).get(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final List<DocumentSnapshot> docs = snapshot.data!.docs;
+            return GridView.builder(
+                itemCount: docs.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 0.7),
+                itemBuilder: (context, index) {
+                  // final doc = docs[index].data();
+                  final doc = EqubModel.fromDocument(docs[index]);
+                  return Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [Text(doc.groupName ?? '')],
+                    ),
+                  );
+                });
+          }),
     );
   }
 }
