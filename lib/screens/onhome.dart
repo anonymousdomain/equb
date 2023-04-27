22fa6cb6ef422efffcaa18bf7e4aeaab79245fe5
anonymousdomain <dawityitagesu@gmail.com>
2023-04-27 12:56:33 +0300

    just playing around

--- lib/screens/onhome.dart
+++ lib/screens/onhome.dart
@@ -15,59 +15,123 @@ class OnHome extends StatefulWidget {
 
 class _OnHomeState extends State<OnHome> {
   List<String> categories = [
-    'Recent',
-    'Popular',
+    'Drivers',
+    'Employee',
     'Trending',
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: Text('MultipleScrolling Experince'),
-          ),
-          for (String category in categories)
-            SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  SizedBox(height: 8),
-                  Container(
-                    height: 200,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 20,
-                      itemBuilder: ((context, index) {
-                        return Container(
-                          width: 160,
-                          margin: EdgeInsets.symmetric(horizontal: 8),
-                          child: Card(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(height: 8),
-                                Text('Item ${index + 1}',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold)),
-                                SizedBox(height: 4),
-                                Text('Category: $category',
-                                    style: TextStyle(fontSize: 14)),
-                                SizedBox(height: 4),
-                                Text('Description of Item ${index + 1}',
-                                    style: TextStyle(fontSize: 14)),
-                              ],
-                            ),
-                          ),
-                        );
-                      }),
-                    ),
+      body: StreamBuilder(
+        stream: groupCollection.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          List<Widget> slivers = [];
+          for (DocumentSnapshot doc in snapshot.data!.docs) {
+            String catagory = doc['catagory'];
+            if (categories.contains(catagory)) {
+              slivers.add(
+                SliverToBoxAdapter(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 8),
+                      SizedBox(
+                        height: 200,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 20,
+                          itemBuilder: ((context, index) {
+                            return Container(
+                              width: 160,
+                              margin: EdgeInsets.symmetric(horizontal: 8),
+                              child: Card(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 8),
+                                    Text('Item ${index + 1}',
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold)),
+                                    SizedBox(height: 4),
+                                    Text('Category: $catagory',
+                                        style: TextStyle(fontSize: 14)),
+                                    SizedBox(height: 4),
+                                    Text('Description of Item ${index + 1}',
+                                        style: TextStyle(fontSize: 14)),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
+              );
+            }
+          }
+          return CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                title: Text('MultipleScrolling Experince'),
               ),
-            ),
-        ],
+              ...slivers
+            ],
+          );
+        },
       ),
+      // body: CustomScrollView(
+      //   slivers: [
+      //     SliverAppBar(
+      //       title: Text('MultipleScrolling Experince'),
+      //     ),
+      //     for (String category in categories)
+      //       SliverToBoxAdapter(
+      //         child: Column(
+      //           children: [
+      //             SizedBox(height: 8),
+      //             Container(
+      //               height: 200,
+      //               child: ListView.builder(
+      //                 scrollDirection: Axis.horizontal,
+      //                 itemCount: 20,
+      //                 itemBuilder: ((context, index) {
+      //                   return Container(
+      //                     width: 160,
+      //                     margin: EdgeInsets.symmetric(horizontal: 8),
+      //                     child: Card(
+      //                       child: Column(
+      //                         crossAxisAlignment: CrossAxisAlignment.start,
+      //                         children: [
+      //                           SizedBox(height: 8),
+      //                           Text('Item ${index + 1}',
+      //                               style: TextStyle(
+      //                                   fontSize: 18,
+      //                                   fontWeight: FontWeight.bold)),
+      //                           SizedBox(height: 4),
+      //                           Text('Category: $category',
+      //                               style: TextStyle(fontSize: 14)),
+      //                           SizedBox(height: 4),
+      //                           Text('Description of Item ${index + 1}',
+      //                               style: TextStyle(fontSize: 14)),
+      //                         ],
+      //                       ),
+      //                     ),
+      //                   );
+      //                 }),
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //   ],
+      // ),
     );
   }
 }
