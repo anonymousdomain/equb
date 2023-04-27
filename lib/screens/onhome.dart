d58f49e185a0f60280ab790e2fa82c3c1ce127a4
anonymousdomain <dawityitagesu@gmail.com>
2023-04-27 12:27:24 +0300

    list of cards with multiple scrolling experience

--- lib/screens/onhome.dart
+++ lib/screens/onhome.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equb/helper/firbasereference.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:intl/intl.dart';
+
+import 'equbGroup/equbs_in.dart';
 
 class OnHome extends StatefulWidget {
   const OnHome({super.key});
@@ -10,35 +14,60 @@ class OnHome extends StatefulWidget {
 }
 
 class _OnHomeState extends State<OnHome> {
+  List<String> categories = [
+    'Recent',
+    'Popular',
+    'Trending',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-          future: groupCollection.get(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final List<DocumentSnapshot> docs = snapshot.data!.docs;
-            return GridView.builder(
-              scrollDirection:Axis.horizontal,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1, childAspectRatio: 1.0),
-                itemBuilder: (context, snapshot) {
-                  return GestureDetector(
-                    onTap: () {},
-                    child: Card(
-                      borderOnForeground: true,
-                      margin: EdgeInsets.all(10),
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('MultipleScrolling Experince'),
+          ),
+          for (String category in categories)
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  SizedBox(height: 8),
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 20,
+                      itemBuilder: ((context, index) {
+                        return Container(
+                          width: 160,
+                          margin: EdgeInsets.symmetric(horizontal: 8),
+                          child: Card(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 8),
+                                Text('Item ${index + 1}',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(height: 4),
+                                Text('Category: $category',
+                                    style: TextStyle(fontSize: 14)),
+                                SizedBox(height: 4),
+                                Text('Description of Item ${index + 1}',
+                                    style: TextStyle(fontSize: 14)),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
                     ),
-                  );
-                });
-          }),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
