c838902354fd632f5ec01cfd20aa664138529c57
anonymousdomain <dawityitagesu@gmail.com>
2023-05-07 22:26:39 +0300

    build results

--- lib/service/search.dart
+++ lib/service/search.dart
@@ -46,11 +46,121 @@ class GroupSearch extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Text('data');
+    if (query.isEmpty) {
+      return Container();
+    }
+    return StreamBuilder(
+      stream: groupCollection.snapshots(),
+      builder: ((context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData) {
+          return Center(child: Text('nodata'));
+        }
+        final List<DocumentSnapshot> docs = snapshot.data!.docs
+            .where((doc) => doc.get('groupName').toLowerCase().contains(query))
+            .toList();
+        return ListView.builder(
+          itemCount: docs.length,
+          itemBuilder: ((context, index) {
+            final members = List<String>.from(docs[index].get('members'));
+
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: members.contains(user?.uid)
+                  ? ListTile(
+                      leading: IconButton(
+                          onPressed: () {}, icon: Icon(FeatherIcons.user)),
+                      title: Text(
+                        docs[index].get('groupName'),
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.headline1!.color),
+                      ),
+                      subtitle: Text(
+                        docs[index].get('catagory'),
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.headline1!.color),
+                      ),
+                      onTap: () {
+                        query = docs[index].get('groupName');
+                      },
+                      trailing: Text(
+                        '${docs[index].get('members').toList().length.toString()} members ',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )
+                  : ListTile(
+                      leading: IconButton(
+                        onPressed: () async {
+                          await requestJoinGroup(docs[index].get('groupId'))
+                              .then(
+                                (value) =>
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: CustomSnackBar(
+                                        message:
+                                            'You Are Requested to Join Group',
+                                        isSuccess: true),
+                                  ),
+                                ),
+                              )
+                              .then(
+                                (value) => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: ((context) => Home()),
+                                  ),
+                                ),
+                              );
+                        },
+                        icon: Icon(
+                          FeatherIcons.plusCircle,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      title: Text(
+                        docs[index].get('groupName'),
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.headline1!.color),
+                      ),
+                      subtitle: Text(
+                        docs[index].get('catagory'),
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.headline1!.color),
+                      ),
+                      trailing: Text(
+                        '${docs[index].get('members').toList().length.toString()} members ',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+            );
+          }),
+        );
+      }),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+     if (query.isEmpty) {
+      return Container();
+    }
     return StreamBuilder(
       stream: groupCollection.snapshots(),
       builder: ((context, snapshot) {
