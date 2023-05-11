3d488cdda3252b32ee489cb4560280e806e1aafd
anonymousdomain <dawityitagesu@gmail.com>
2023-05-11 08:50:58 +0300

    trying to show a group name but faild with error of snapshot platform

--- lib/screens/equbGroup/requested_groups.dart
+++ lib/screens/equbGroup/requested_groups.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equb/helper/firbasereference.dart';
 import 'package:flutter/material.dart';
@@ -36,10 +38,16 @@ class _GroupRequestState extends State<GroupRequest> {
           }
           List<Future<DocumentSnapshot>> userDocFuture = [];
           snapshot.data!.docs.forEach((doc) {
+            String groupId = doc.id;
+            log('groupId $groupId');
             List<String> usersId =
                 List<String>.from(doc.data()['groupRequest']);
             usersId.forEach((id) {
-              userDocFuture.add(userCollection.doc(id).get());
+              userDocFuture.add(userCollection.doc(id).get().then((userDoc) {
+                userDoc.data()!['groupId'] = groupId;
+                log(userDoc.data().toString());
+                return userDoc;
+              }));
             });
           });
 
@@ -57,23 +65,50 @@ class _GroupRequestState extends State<GroupRequest> {
                 );
               }
               List<DocumentSnapshot> userDocs = snapshot.data!;
-              // List<DocumentSnapshot> docs = snapshot.data!.docs;
-              return ListView.builder(
-                itemCount: userDocs.length,
-                itemBuilder: ((context, index) {
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: ListTile(
-                      leading: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 8),
-                          child: Text('requested')),
-                      title: Text(userDocs[index].get('firstName')),
-                      trailing: Text('requested group'),
-                    ),
-                  );
-                }),
+              return SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  itemCount: userDocs.length,
+                  itemBuilder: ((context, index) {
+                    String groupId = userDocs[index].get('uid');
+                    return SizedBox(
+                      width: 600,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Stack(children: [
+                          ListTile(
+                            leading: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 8),
+                                child: Text('requested')),
+                            title: Text(userDocs[index].get('firstName') ?? ''),
+                          ),
+                          // Align(
+                          //   alignment: Alignment.bottomRight,
+                          //   child: FutureBuilder(
+                          //       future: groupCollection.doc(groupId).get(),
+                          //       builder: ((context, snapshot) {
+                          //         if (snapshot.connectionState ==
+                          //             ConnectionState.waiting) {
+                          //           return Center(
+                          //             child: CircularProgressIndicator(),
+                          //           );
+                          //         }
+                          //         if (!snapshot.hasData) {
+                          //           return Center(
+                          //             child: Text('no data'),
+                          //           );
+                          //         }
+                          //         // List<DocumentSnapshot> groupDoc = snapshot.data;
+                          //         return Text(snapshot.data?.get('id'));
+                          //       })),
+                          // )
+                        ]),
+                      ),
+                    );
+                  }),
+                ),
               );
             }),
           );
