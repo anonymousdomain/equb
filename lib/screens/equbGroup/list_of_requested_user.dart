c42a4d878524eba040541c983ce68292195b394b
anonymousdomain <dawityitagesu@gmail.com>
2023-05-19 01:55:09 +0300

    show list of users requested in a group

---
+++ lib/screens/equbGroup/list_of_requested_user.dart
@@ -0,0 +1,129 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equb/helper/firbasereference.dart';
+import 'package:equb/service/group.dart';
+import 'package:equb/widget/custom_snackbar.dart';
+import 'package:equb/widget/customtext_iconbutton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+class ListOfUsersRequested extends StatefulWidget {
+  ListOfUsersRequested(
+      {required this.userIds, required this.groupId, super.key});
+  List<String> userIds;
+  String groupId;
+  @override
+  State<ListOfUsersRequested> createState() => _ListOfUsersRequestedState();
+}
+
+class _ListOfUsersRequestedState extends State<ListOfUsersRequested> {
+  Future<List<DocumentSnapshot>> getUserDocs(userFuture) async {
+    return await Future.wait(userFuture);
+  }
+
+  List<Future<DocumentSnapshot>> userDocFuture = [];
+  Future<void> _approve(groupId, userId) async {
+    approveRequest(groupId, userId)
+        .then((value) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            content: CustomSnackBar(
+              isSuccess: true,
+              message: 'User request approved to a group successfuly',
+            ))))
+        .then((value) => Navigator.of(context).pop());
+  }
+
+  Future<void> _cancel(groupId, userId) async {
+    cancelRequest(groupId, userId)
+        .then((value) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            content: CustomSnackBar(
+              isSuccess: false,
+              message: 'user requested is cancled',
+            ))))
+        .then((value) => Navigator.of(context).pop());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    for (var id in widget.userIds) {
+      userDocFuture.add(userCollection.doc(id).get());
+    }
+    return Scaffold(
+      body: FutureBuilder(
+          future: getUserDocs(userDocFuture),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (!snapshot.hasData) {
+              return Center(
+                child: Text(
+                  'Result Not Found',
+                ),
+              );
+            }
+            List<DocumentSnapshot> usersDoc = snapshot.data ?? [];
+
+            return ListView.builder(
+                itemCount: usersDoc.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: CircleAvatar(
+                                radius: 24,
+                                backgroundColor: Theme.of(context).primaryColor,
+                                backgroundImage: NetworkImage(
+                                    usersDoc[index].get('imageUrl')),
+                              ),
+                            ),
+                            ListTile(
+                              title: Text('fullName'),
+                              trailing: Text(
+                                  '${usersDoc[index].get("firstName")} ${usersDoc[index].get("lastName")}'),
+                            ),
+                            ListTile(
+                              title: Text('workstatus'),
+                              trailing: Text('employee'),
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                CustomTextButtonIcon(
+                                    icon: FeatherIcons.xOctagon,
+                                    color: Colors.red,
+                                    text: 'cancel',
+                                    ontap: () {
+                                      _cancel(widget.groupId,usersDoc[index].get('uid'));
+                                    }),
+                                CustomTextButtonIcon(
+                                    icon: FeatherIcons.checkCircle,
+                                    color: Colors.blue,
+                                    text: 'approve',
+                                    ontap: () {
+                                      _approve(widget.groupId,
+                                          usersDoc[index].get('uid'));
+                                    })
+                              ],
+                            )
+                          ],
+                        )),
+                  );
+                });
+          }),
+    );
+  }
+}
