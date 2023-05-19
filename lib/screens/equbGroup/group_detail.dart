09ccecda37bc1a9784bc37c78b9c3c7881ba6a37
anonymousdomain <dawityitagesu@gmail.com>
2023-05-20 02:24:05 +0300

    group detail implemented

--- lib/screens/equbGroup/group_detail.dart
+++ lib/screens/equbGroup/group_detail.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equb/helper/firbasereference.dart';
 import 'package:equb/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../../helper/images.dart';
 import '../../service/services.dart';
 
 class GroupsDetail extends StatefulWidget {
@@ -50,56 +53,93 @@ class _GroupsDetailState extends State<GroupsDetail> {
                   ),
                 );
               }
-              Object docs = snapshot.data ?? [];
+              DocumentSnapshot<Map<String, dynamic>> docs = snapshot.data!;
               return CustomScrollView(
                 slivers: [
                   SliverAppBar(
-                    expandedHeight: 200,
+                    expandedHeight: 160,
                     flexibleSpace: FlexibleSpaceBar(
-                      background: CircleAvatar(
-                        backgroundImage: NetworkImage(_user?.imageUrl ?? ''),
-                      ),
+                      background: SvgPicture.asset(Images.save,fit:BoxFit.cover,),
                       titlePadding:
-                          EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                          EdgeInsets.symmetric(horizontal: 0, vertical: 0),
                       title: Align(
-                        alignment: Alignment.bottomCenter,
+                        alignment: Alignment.center,
                         child: Text(
-                          '${_user?.firstName} ${_user?.lastName ?? ''}',
+                          '${docs.get('groupName')} group',
                           style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontSize: 15),
+                              color:Theme.of(context).primaryColor,
+                              fontSize: 32),
                         ),
                       ),
                       centerTitle: true,
                     ),
+                    bottom: PreferredSize(
+                        preferredSize: Size.fromHeight(200),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 40,
+                              backgroundImage:
+                                  NetworkImage(_user!.imageUrl ?? ''),
+                            ),
+                            Text(
+                              '${_user!.firstName} ${_user!.lastName}',
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 24),
+                            )
+                          ],
+                        )),
                   ),
                   SliverToBoxAdapter(
                     child: SizedBox(
-                      height:10,
+                      height: 10,
                     ),
                   ),
                   SliverToBoxAdapter(
-                    child: GridView.builder(
-                      clipBehavior:Clip.antiAliasWithSaveLayer,
-                        itemCount: 6,
+                    child: GridView.count(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 12.0),
                         shrinkWrap: true,
-                        physics: NeverScrollableScrollPhysics(),
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio:1.3
-                        ),
-                        itemBuilder: (context, index) {
-                          return Card(
-                            child: Center(
-                              child: Text('Grid Card $index'),
-                            ),
-                          );
-                        }),
+                        children: [
+                          CustomGRoupCard(
+                            text: 'Payment',
+                          ),
+                          CustomGRoupCard(
+                            text: 'Members',
+                          ),
+                          CustomGRoupCard(
+                            text: 'Eta',
+                          ),
+                          CustomGRoupCard(
+                            text: 'Completed Equb',
+                          ),
+                        ]),
                   )
                 ],
               );
             }),
       ),
+    );
+  }
+}
+
+class CustomGRoupCard extends StatelessWidget {
+  CustomGRoupCard({
+    required this.text,
+    super.key,
+  });
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Center(child: Text(text,style:TextStyle(
+        color: Theme.of(context).textTheme.headline1!.color,
+        fontSize: 15,
+      ),)),
     );
   }
 }
